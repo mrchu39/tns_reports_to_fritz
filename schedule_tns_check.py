7d28ecd86c8c209dd21e6fc033e87cbfa6137255
mrchu39 <mrchu39@berkeley.edu>
2021-12-21 18:21:08 -0800

    Update schedule_tns_check.py

--- schedule_tns_check.py
+++ schedule_tns_check.py
@@ -9,10 +9,10 @@ num = 0
 while(True):
     if datetime.datetime.utcnow().hour == 0 and num != 1:
         get_at_reports.submit_tns_alerts(3)
-        first = 1
+        num = 1
 
     if datetime.datetime.utcnow().hour == 12 and num != 2:
         get_at_reports.submit_tns_alerts(3)
-        first == 2
+        num == 2
         
     time.sleep(1)
